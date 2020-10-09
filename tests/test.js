8f05d8b62b4e4b3fb729f9b3a7ed6a226ba63731
Abhishek Nair <abhisheknair135@gmail.com>
2020-10-09 14:19:53 +0530

    Written tests for admin module

--- tests/test.js
+++ tests/test.js
@@ -129,3 +129,118 @@ describe("Test the /agent routes", () => {
   });
 
 });
+
+
+describe("Test all /admin routes", () => {
+  var token;
+  before((done) => {
+    chai
+      .request(server)
+      .post("/login")
+      .send({
+        "userName": "param@gmail.com",
+        "password": "param",
+        "role": "ADMIN"
+    })
+      .end(function (err, res) {
+        if (err) throw err;
+        token = res.body.token;
+        done();
+      });
+  });
+
+  it("Should GET all the loan contracts", (done) => {
+    chai
+      .request(server)
+      .get("/admin/list")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("contracts");
+        res.body.contracts.length.should.be.gte(0);
+        done();
+      });
+  });
+  it("Unauthorized GET request to list all contracts", (done) => {
+    chai
+      .request(server)
+      .get("/admin/list")
+      .end((err, res) => {
+        res.should.have.status(401);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message");
+        done();
+      });
+  });
+  it("Should GET all the loan contracts satisfying the condition(s)", (done) => {
+    chai
+      .request(server)
+      .get("/admin/list?status=APPROVED&startDate=2020-10-08")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("contracts");
+        res.body.contracts.length.should.be.gte(0);
+        done();
+      });
+  });
+
+  it("Should GET particular contract", (done) => {
+    chai
+      .request(server)
+      .get("/admin/view/5f7f56b632c90a416076581a")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("contract");
+        Object.keys(res.body.contract).length.should.be.gte(0);
+        done();
+      });
+  });
+  it("Should edit existing contract", (done) => {
+    chai
+      .request(server)
+      .patch("/admin/edit?interestRate=9&noOfYears=2&contractId=5f801726a9695c4098929471")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message");
+        res.body.noOfUpdates.should.be.gte(0);
+        done();
+      });
+  });
+  it("Should throw error for non - existing contract", (done) => {
+    chai
+      .request(server)
+      .patch("/admin/edit?interestRate=8&noOfYears=3&contractId=5f801726a9695c409892947")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(500);
+        res.body.should.be.a("object");
+        res.body.should.have.property("error");
+        done();
+      });
+  });
+
+  it("Should approve a contract", (done) => {
+    chai
+      .request(server)
+      .patch("/admin/approve/5f801726a9695c4098929471")
+      .set("Authorization", "bearer " + token)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.body.should.be.a("object");
+        res.body.should.have.property("message");
+        res.body.noOfUpdates.should.be.gte(0);
+        done();
+      });
+  });
+  
+  it("close connection", () => {
+    mongoose.connection.close();
+  });
+})
